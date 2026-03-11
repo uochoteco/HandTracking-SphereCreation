e8070ea245a68242557ad14ebfe1116fe05f8a69
uochoteco <unax.ochoteco@icloud.com>
2026-03-10 21:20:51 -0500

    Huge Progress
    Initial setup done

--- src/main/java/com/uochoteco/Main.java
+++ src/main/java/com/uochoteco/Main.java
@@ -1,7 +1,10 @@
 package com.uochoteco;
-
+import org.opencv.core.Core;
+import  nu.pattern.OpenCV;
 public class Main {
     public static void main(String[] args) {
+        OpenCV.loadShared();
         System.out.println("Hello world!");
+        System.out.println("Version: " + Core.VERSION);
     }
 }
